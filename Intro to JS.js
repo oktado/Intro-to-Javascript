62dc3b474ceff2c39a1c3dd8d78dbedd3560ffe9
Oktado Putra <oktado.putra@gmail.com>
2020-10-20 08:14:42 +0700

    Membuat file intro to JS

---
+++ Intro to JS.js
@@ -0,0 +1,41 @@
+// Exercise 1
+
+// console.log("Oktado");
+// console.log("Malang");
+// console.log("7 Oktober 1990");
+// console.log("30 tahun");
+// console.log("Web Developer");
+
+// Exercise 2
+/*
+prompt("Nama User :");
+prompt("Asal Negara :");
+confirm('Apakah data yang anda isi sudah benar ?')
+alert('Terimakasih sudah mengisi form ini')
+*/
+
+// Exercise 5
+
+let nama = ('Oktado')
+let asal = ('Malang')
+let tglLahir = ('7 Oktober 1990')
+let umur = ('30 tahun')
+let pekerjaan = ('Web Developer')
+
+console.log('Halo nama saya '+ nama);
+console.log('Saya lahir di '+ asal);
+console.log('Lahir pada tanggal '+ tglLahir);
+console.log('Sekarang saya berusia '+ umur);
+console.log('Saya berprofesi sebagai '+ pekerjaan);
+
+// Exercise 6
+
+var hitung1 = 25+10;
+var hitung2 = 100*2;
+var hitung3 = 99/2;
+var hitung4 = 99%2;
+
+console.log('Hasil dari 25 + 30 adalah '+hitung1);
+console.log('Hasil dari 100 * 2 adalah '+hitung2);
+console.log('Hasil dari 99 / 2 adalah '+hitung3);
+console.log('Hasil dari 99 % 2 adalah '+hitung4);
